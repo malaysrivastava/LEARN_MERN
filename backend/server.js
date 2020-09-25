79086abf58b726992947a89d8686157682fde9ac
Malay Srivastava <57169971+malaysrivastava@users.noreply.github.com>
2020-09-25 23:28:21 +0530

    learnt MERN

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,{ useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log("Connected to Mongodb Atlas successfully");
+})
+
+const exercisesRouter = require('./routes/exercise');
+const usersRouter = require('./routes/user');
+
+app.use('/exercises',exercisesRouter);
+app.use('/users',usersRouter);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port: ${PORT}`);
+});
